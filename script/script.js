bfd00fea971456f39849744035f5f718d56b1947
Simo Schmidt <simosch7@gmail.com>
2024-07-18 14:10:22 +0300

    Add hover effect & CSS changes
    Added hover effect with mouse (still requires work
    to only occur when mousebutton is also pressed).
    Added CSS changes - center grid in the middle of
    the page, and made the grid boxes dynammically re-
    size based on how big the grid is.
     Changes to be committed:
    	modified:   index.html
    	modified:   script/script.js
    	modified:   style/styles.css

--- script/script.js
+++ script/script.js
@@ -1,18 +1,38 @@
 const container = document.querySelector("#container");
 
-gridSize = 16;
 //let gridSize = prompt('What grid size would you like?');
 //change this later to a slider?
-
-for (let i = 0; i < gridSize; i++) {
-  const column = document.createElement("div");
-  //columnDiv.textContent = '-c' + (i + 1).toString();
-  container.appendChild(column);
-  
+function createGrid (userGrid) {
+  let gridSize = userGrid;
   for (let i = 0; i < gridSize; i++) {
-  const rowDiv = document.createElement("div");
-  rowDiv.textContent = '-r' + (i + 1).toString();
-  rowDiv.classList.add("rowDiv");
-  column.appendChild(rowDiv);
+    const column = document.createElement("div");
+    column.classList.add("columnDiv");
+    //columnDiv.textContent = '-c' + (i + 1).toString();
+    container.appendChild(column);
+    
+    for (let j = 0; j < gridSize; j++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.textContent = '';
+    rowDiv.classList.add("rowDiv");
+    //rowDiv.textContent = '-r' + (j).toString();
+    column.appendChild(rowDiv);
+      
+      rowDiv.addEventListener("mouseover", () => {
+        rowDiv.style.backgroundColor = "red";
+      });
+    }
   }
 }
+
+createGrid (8);
+
+/*
+One thing to be careful of with this approach is when the mouse is released somewhere outside the target element. In this case you may want to include an additional mouseleave event to also remove the added listener or add mouseup to the window object so that it can detect when the mouse is released outside the target element.
+
+gridContainer.addEventListener("mousedown", () => {
+  gridContainer.addEventListener("mouseover", coloring);
+});
+window.addEventListener("mouseup", () => {
+  gridContainer.removeEventListener("mouseover", coloring);
+});
+*/
